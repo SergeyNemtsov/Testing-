958c1c1202f3bf592cf5d30e6d0b4c3492b1c9ed
SergeyNemtsov <117725360+SergeyNemtsov@users.noreply.github.com>
2023-01-24 22:19:29 +0300

    Add files via upload

--- test3/searchTesla.js
+++ test3/searchTesla.js
@@ -1,47 +1,44 @@
 module.exports = {
-    before(browser) {
-    browser
-    .page.Tesla()
-    .navigate()
-    .waitForElementVisible('@catalog', 'Catalog title visible')
-    .assert.textContains('@catalog', 'Каталог', 'Catalog ok')
-},
+  before(browser) {
+    browser.page
+      .Tesla()
+      .navigate()
+      .waitForElementVisible("@catalog", "Catalog title visible")
+      .assert.textContains("@catalog", "Каталог", "Catalog ok");
+  },
 
-    after(browser) {
+  after(browser) {
     browser.end();
-    },
+  },
 
+  "Search for Tesla": function (browser) {
+    browser.page
+      .Tesla()
+      .search("Tesla")
+      .waitForElementVisible("@pageTitle", "Tesla title here");
 
-    'Search for Tesla': function (browser) {
-    browser
-    .page.Tesla()
-    .search('Tesla')
-    .waitForElementVisible('@pageTitle', 'Tesla title here');
+    browser.page.Tesla().strictClick("@searchBar");
 
-    browser.strictClick('@searchBar')
-    
-    .assert.title('TestGym', 'title ok')
-    .assert.textContains('@pageTitle', 'Tesla car XXXX III' || 'Tesla car X', 'Tesla title ok')
+    browser.page
+      .Tesla()
+      .assert.title("TestGym", "title ok")
+      .assert.textContains(
+        "@pageTitle",
+        "Tesla car XXXX III" || "Tesla car X",
+        "Tesla title ok"
+      );
 
-    browser .page.Tesla() 
-    .section.topMenu
-    .assert.textContains('@main', 'Главная');
-
-    
+    browser.page
+      .Tesla()
+      .section.topMenu.assert.textContains("@main", "Главная");
 
     let word = browser.globals.searchText;
 
-    browser.page.Tesla()
-.search(word)
-.waitForElementVisible('@pageTitle','Page title here')
-.assert.title(word,'title ok')
-.assert.textContains('@pageTitle', word,word+' TestGym');
-    },
+    browser.page
+      .Tesla()
+      .search(word)
+      .waitForElementVisible("@pageTitle", "Page title here")
+      .assert.title(word, "TestGym")
+      .assert.textContains("@pageTitle", word, word + "TestGym");
+  },
 };
-
-
-
-
-
-
-    
