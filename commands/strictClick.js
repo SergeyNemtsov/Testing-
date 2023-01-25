eed5b3968edd5dbc108e7e0afddbe098d0cca3ad
SergeyNemtsov <117725360+SergeyNemtsov@users.noreply.github.com>
2023-01-25 21:12:41 +0300

    Delete commands directory

--- commands/strictClick.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    command: function(selector) {
-    return this
-    .waitForElementVisible({selector: '//body/div[1]/div[1]/div[1]', locateStrategy: 'xpath'})
-    .click('input[type="text"]');
-    }
-    };
